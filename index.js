cb757c6588fb2481bf26e2de8aa132a12c2a6c4c
clarababette <clara_coetzee@zoho.com>
2022-05-17 12:53:23 +0200

    ssl error

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ function get_PostgreSQL_connection() {
 }
 
 const pgp = PgPromise({});
-
-const db = pgp(get_PostgreSQL_connection());
+const db = pgp(DATABASE_URL);
+//const db = pgp(get_PostgreSQL_connection());
 
 const API = APIRoutes(db);
 
