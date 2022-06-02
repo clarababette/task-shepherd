4d0d73e4f11975c7d8737964b6a3ec29fbc902f2
clarababette <clara_coetzee@zoho.com>
2022-06-02 15:18:35 +0200

    api update

--- services.js
+++ services.js
@@ -11,11 +11,9 @@ function APIRoutes(db) {
     if (result.length < 1) {
       result = await db.any('select * from mentors where email = $1', email);
       if (result.length > 0) {
-        user = task;
         user.role = 'mentor'
 }
     } else {
-      user = task;
         user.role = 'coder'
     }
     res.json(user)
