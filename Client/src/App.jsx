7c8639beb93df5eaea10fe45f030a67702723b3c
clarababette <clara_coetzee@zoho.com>
2022-06-13 16:17:08 +0200

    lazy loading role pages

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,39 +1,51 @@
 
-import { useState, useContext } from 'react';
-import UserContext from './context/UserContext';
+import React, { useState, useContext, Suspense } from 'react';
 import AppContext from './context/AppContext';
-import CoderPage from './coder_view/CoderPage';
-import MentorPage from './mentor_view/MentorPage';
+import UserContext from './context/UserContext';
 import './styles/mui-styles.scss';
 import Header from './Header';
 import { CircularProgress, Box } from '@mui/material';
 
+const CoderPage = React.lazy(() => import('./coder_view/CoderPage'));
+const MentorPage = React.lazy(() => import('./mentor_view/MentorPage'));
 
 function App() {
-  const { setEmail, user, loading, setLoading } = useContext(UserContext);
+  const { colors } = useContext(AppContext) 
+  const { setEmail, user } = useContext(UserContext);
   const [emailInput, setEmailInput] = useState();
-  const { colors } = useContext(AppContext)
 
-  
-  
-  return (
-      <Box className="app"> 
-      {loading && <Box sx={{width:'100vw', height: '100vh', backgroundColor: colors.blue.dark}}><CircularProgress size={100} sx={{color: colors.blue.light, margin:'calc(50vh - 50px) calc(50vw - 50px)'}}></CircularProgress> </Box>}
-      {!loading && !user && <form onSubmit={(e) => {
+  const loading = <Box sx={{width:'100vw', height: '100vh', backgroundColor: 'blue'}}><CircularProgress size={100} sx={{color: 'white', margin:'calc(50vh - 50px) calc(50vw - 50px)'}}></CircularProgress> </Box>
+
+  if (!user) {
+    return (<form onSubmit={(e) => {
           e.preventDefault();
-          setLoading(true);
           setEmail(emailInput);
       }}>
         <h2>Enter email</h2>
         <input type='text' onChange={(e) => { setEmailInput(e.target.value) }} />
         <input type='submit'></input>
-      </form>}
-
-      {!loading && user && <Header name={`${user.first_name} ${user.last_name}`} role={ user.role}/>}
-      {!loading && user && user.role == 'mentor' && <MentorPage />}
-      {!loading && user && user.role == 'coder' && <CoderPage />}
+      </form>)
+  } else if (user.role == 'mentor') {
+    return (
+    <Suspense fallback={loading}>
+    <Box className="app"> 
+      <Header name={`${user.first_name} ${user.last_name}`} role={ user.role}/>
+      <MentorPage />
       </Box>
-  );
+    </Suspense>)
+  } else if (user.role == 'coder') {
+    return(
+    <Suspense fallback={loading}>
+    <Box className="app"> 
+      <Header name={`${user.first_name} ${user.last_name}`} role={ user.role}/>
+      <CoderPage />
+      </Box>
+    </Suspense>)
+  } else {
+ 		return <div> Invalid User </div>;
 }
+
+  }
+
 
 export default App;
