a9b8f586ff670f9973349a684f5c75ae8767bc55
clarababette <clara_coetzee@zoho.com>
2022-06-14 11:34:58 +0200

    a commit

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ const PORT = process.env.PORT || 4017;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 
 const { DATABASE_URL } = process.env;
