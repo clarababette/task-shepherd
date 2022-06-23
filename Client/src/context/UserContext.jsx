4386b6599ed3a65a6ba965d75ee363868d6a72eb
clarababette <clara_coetzee@zoho.com>
2022-06-23 11:48:23 +0200

    auth update

--- Client/src/context/UserContext.jsx
+++ Client/src/context/UserContext.jsx
@@ -11,7 +11,7 @@ export const UserProvider = ({ children }) => {
 
   useEffect(async () => {
     if(email){
-      await axios.post(`/login/auth`).then((res) => {
+      await axios.get(`/login/auth`).then((res) => {
         setUser(res.data)
         setLoading(false)
       });
